0ad5b6af2584b342bb1c9509d6d4c1bdc446c668
frol_73 <frolovpavel73@yandex.ru>
2019-07-12 21:43:29 +0400

    CD-1 Add base configurations

---
+++ configurations/get_env.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+
+class GetData:
+    dotenv_path = os.path.dirname(__file__) + "/env"
+    load_dotenv(dotenv_path)
+
+    @staticmethod
+    def gat_data(value: str) -> str:
+        return os.environ[value]
