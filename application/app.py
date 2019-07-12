009d1e9078751ebed7947352065d5344e979bbba
frol_73 <frolovpavel73@yandex.ru>
2019-07-12 21:46:14 +0400

    CD-1 Add base objects for application

---
+++ application/app.py
@@ -0,0 +1,22 @@
+from flask import (
+    Flask,
+)
+
+from configurations.run_params import Configuration
+from flask_sqlalchemy import (
+    SQLAlchemy,
+)
+from flask_migrate import (
+    Migrate,
+    MigrateCommand,
+)
+from flask_script import Manager
+
+app = Flask(__name__)
+app.config.from_object(Configuration)
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
